7a4ee641e5d7562fd6f0ad948a4e8e15de082736
Agnivesh Arya <15124142+agnivesh29@users.noreply.github.com>
2018-08-19 00:00:18 +1000

    Get bucket and file details from codepipeline and deploy to s3

--- Codebuild_Lambda_Deploy.py
+++ Codebuild_Lambda_Deploy.py
@@ -8,11 +8,22 @@ def lambda_handler(event, context):
     s3=boto3.resource('s3')
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:ap-southeast-2:586861109696:PortfolioDeployCompleted')
+    job = event.get('CodePipeline.job')
+    location = {
+        'bucketName': 'portfolio.codebuild.pipeline',
+        'objectKey': 'portfoliobuild.zip'
+    }
+    if job:
+        for artifact in job['data']['inputArtifacts']:
+            if artifact['Name'] == 'MyAppBuild':
+                location = artifact['location']['s3Location']
+
+    print('Building portfolio from {}'.format(str(location)))
     try:
-        codebuild_buccket = s3.Bucket('portfolio.codebuild.pipeline')
+        codebuild_buccket = s3.Bucket(location['bucketName'])
         portfolio_bucket = s3.Bucket('agni-portfolio')
         portfolio_zip = io.BytesIO()
-        codebuild_file=codebuild_buccket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        codebuild_file=codebuild_buccket.download_fileobj(location['objectKey'], portfolio_zip)
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for name in myzip.namelist():
                 obj = myzip.open(name)
@@ -20,6 +31,9 @@ def lambda_handler(event, context):
                 ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
                 portfolio_bucket.Object(name).Acl().put(ACL='public-read')
         topic.publish(Subject='Portfolio deployed successfully',Message='Congratulation !!! Lambda has successfully deployed the portfolio to s3')
+        if job:
+            code_pipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job['Id'])
     except:
         topic.publish(Subject='Code deploy failed', Message='Code deployment to S3 via Lambda failed')
         raise;
