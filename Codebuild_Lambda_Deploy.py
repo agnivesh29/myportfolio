f0dc222891f5458eaca1c8491b6b347dcf25fcff
Agnivesh Arya <15124142+agnivesh29@users.noreply.github.com>
2018-08-18 21:21:11 +1000

    updated file as per lambda requiremnt

--- Codebuild_Lambda_Deploy.py
+++ Codebuild_Lambda_Deploy.py
@@ -1,20 +1,20 @@
-# set AWS_PROFILE=aws_personal
 import boto3
 import io
 import zipfile
 import mimetypes
 
-s3=boto3.resource('s3')
-codebuild_buccket = s3.Bucket('portfolio.codebuild.pipeline')
-portfolio_bucket = s3.Bucket('agni-portfolio')
+def lambda_handler(event, context):
+    # set AWS_PROFILE=aws_personal
+    s3=boto3.resource('s3')
+    codebuild_buccket = s3.Bucket('portfolio.codebuild.pipeline')
+    portfolio_bucket = s3.Bucket('agni-portfolio')
+    portfolio_zip = io.BytesIO()
+    codebuild_file=codebuild_buccket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for name in myzip.namelist():
+            obj = myzip.open(name)
+            portfolio_bucket.upload_fileobj(obj,name,
+            ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+            portfolio_bucket.Object(name).Acl().put(ACL='public-read')
 
-# codebuild_file=codebuild_buccket.download_file('portfoliobuild.zip','C:/Users/AArya/Downloads/Temp/portfoliobuild.zip')
-
-portfolio_zip = io.BytesIO()
-codebuild_file=codebuild_buccket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for name in myzip.namelist():
-        obj = myzip.open(name)
-        portfolio_bucket.upload_fileobj(obj,name,
-        ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
-        portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+    return 'Hello from Lambda'
